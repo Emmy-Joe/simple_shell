86d698366c9d6a985c1d1b7efe73a00a1ca6f985
codemidev <ochaijay81@gmail.com>
2022-08-28 00:00:26 +0100

    Prompt

---
+++ prompt.c
@@ -0,0 +1,18 @@
+//Example7.c
+#include <stdio.h>
+
+int main()
+{
+	char c;
+	printf("Enter 1st character: ");
+	while((c=getchar())=='\n');
+	printf("You Entered : %c\n",c);
+
+	while(getchar()!='\n');
+
+	printf("Enter 2nd character : ");
+	while((c=getchar())=='\n');
+	printf("You Entered : %c\n",c);
+	return 0;
+}
+
